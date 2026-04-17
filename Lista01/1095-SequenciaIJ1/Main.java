fa6fa0b5f355593621d41b695771576327ecf1be
Jhonata Freitas <86995208+ALVlN@users.noreply.github.com>
2026-04-17 04:36:17 -0300

    SoluçãoTask1095

---
+++ Lista01/1095-SequenciaIJ1/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int I = 1;
+
+        for(int i = 60; i >= 0; i -= 5){
+            System.out.printf("I=%d J=%d%n", I, i);
+            I = I + 3;https://github.com/ALVlN/Beecrowd-JavaTasks/projects
+        }
+    }
+
+}
